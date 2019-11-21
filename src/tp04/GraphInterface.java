0472c6c98d26c942dbd41bcf880bfe9934b78fe2
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-11-21 13:34:23 +0100

    update to interface methods 2

--- src/tp04/GraphInterface.java
+++ src/tp04/GraphInterface.java
@@ -6,7 +6,7 @@ public interface GraphInterface {
 	
 	// inutile : public boolean isInGraph(VertexInterface e); 
 	public ArrayList<VertexInterface> getAllVertices();
-	public boolean isThereAWay(VertexInterface a, VertexInterface b); //utilisee ?
+	// inutile : public boolean isThereAWay(VertexInterface a, VertexInterface b); //utilisee ?
 	public int getWeight(VertexInterface src, VertexInterface dst);
 	public ArrayList<VertexInterface> getSuccessors(VertexInterface e);
 	
