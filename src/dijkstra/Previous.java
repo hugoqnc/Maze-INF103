220f6f72bfe78296294a07a6b63519cf3e52bbe5
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-11-21 15:33:25 +0100

    update Previous class

--- src/dijkstra/Previous.java
+++ src/dijkstra/Previous.java
@@ -1,5 +1,16 @@
 package dijkstra;
 
-public class Previous {
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class Previous extends Hashtable<VertexInterface, VertexInterface> {
+	
+	public VertexInterface getValue(VertexInterface e) {
+		
+	}
+	
+	public void setValue(VertexInterface e, VertexInterface p);
+	
+	public ArrayList<VertexInterface> getShortestPathTo(VertexInterface e);
 
 }
