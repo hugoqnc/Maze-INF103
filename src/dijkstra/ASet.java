aa446e34a1728267886a65c336fd45a95d6c1621
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-11-22 16:05:46 +0100

    implement Previous methods and change ASet

--- src/dijkstra/ASet.java
+++ src/dijkstra/ASet.java
@@ -2,11 +2,12 @@ package dijkstra;
 
 import java.util.HashSet;
 
-public class ASet extends HashSet<VertexInterface> {
+public class ASet extends HashSet<VertexInterface> implements ASetInterface{
 	
 	private HashSet<VertexInterface> ensembleA;
 	
 	public ASet() {
+		super();
 		ensembleA = new HashSet<VertexInterface>();
 	}
 	
