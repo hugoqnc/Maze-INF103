8e227e9ecdcc394ec81e4578831d0a1f8bb72e07
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-11-22 16:37:32 +0100

    new Pi class and implementation of its methods

---
+++ src/dijkstra/Pi.java
@@ -0,0 +1,23 @@
+package dijkstra;
+
+import java.util.Hashtable;
+
+public class Pi extends Hashtable<VertexInterface, Integer> implements PiInterface{
+	
+	private Hashtable<VertexInterface, Integer> piTable ;
+	
+	public Pi() {
+		super();
+		piTable = new Hashtable<VertexInterface, Integer>() ;
+	}
+	
+	public int getValue(VertexInterface e) {
+		return piTable.get(e).intValue();
+	}
+	
+	public void setValue(VertexInterface e, int v) {
+		piTable.put(e,v);
+		
+	}
+
+}
