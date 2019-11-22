a85c2d1750a34672e833e60d446d5f91510d546a
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-11-23 00:09:07 +0100

    fix comments

--- src/ihm/Window.java
+++ src/ihm/Window.java
@@ -9,7 +9,7 @@ public class Window extends JFrame {
 		JFrame window = new JFrame();
 		window.setTitle("Labyrinthe binome 6");//nom de la fenetre
 		window.setSize(1000, 800);//taille (en pixel)
-		window.setLocationRelativeTo(null);//position centrée sur l'écran
+		window.setLocationRelativeTo(null);//position centrale sur l'ecran
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//fin sur x
 		window.setResizable(false); //empeche redimentionnement de la fenetre
 		
