21561d44df8e00980b2cc436e0b73bda2a1bb684
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-11-25 11:29:12 +0100

    the maze is displayed in a java interface

---
+++ src/ihm/BoxPanel.java
@@ -0,0 +1,36 @@
+package ihm;
+import maze.*;
+import maze.Box; //pour eviter un conflit avec javax.swing
+import java.awt.*;
+
+import javax.swing.*;
+
+public class BoxPanel extends JPanel {
+	
+	//private Window window;
+	
+	public BoxPanel(Box box /*, Window window*/ ) {
+		super();
+		
+		//this.window = window ;
+		
+		String type = box.status();
+		
+		if (type.contentEquals("A")) {
+			setBackground(Color.RED);
+		}
+		if (type.contentEquals("D")) {
+			setBackground(Color.GREEN);
+		}
+		if (type.contentEquals("W")) {
+			setBackground(Color.BLACK);
+		}
+		if (type.contentEquals("E")) {
+			setBackground(Color.WHITE);
+		}
+		
+		setPreferredSize(new Dimension(10,10));
+		
+	}
+
+}
--- src/ihm/MazePanel.java
+++ src/ihm/MazePanel.java
@@ -1,17 +1,31 @@
 package ihm;
 import javax.swing.*;
 import maze.*;
+import maze.Box;
+
 import java.awt.*;
+import java.util.ArrayList;
 
 public class MazePanel extends JPanel {
 	
+	//private Window window ;
+	
 	public MazePanel(Maze maze) {
 		super();
+		
+		ArrayList<ArrayList<Box>> mazeTable = maze.getMaze();
 		
 		int longueur = maze.getLongeur();
 		int largeur = maze.getLargeur();
 		
 		setLayout(new GridLayout(longueur, largeur));
+		
+		for (int i=0; i<longueur; i++) {
+			for (int j=0; j<largeur; j++) {
+				BoxPanel boxPanel = new BoxPanel(mazeTable.get(i).get(j));
+				add(boxPanel);
+			}
+		}
 
 	}
 	
