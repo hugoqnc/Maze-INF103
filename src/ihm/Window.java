affcdd7f882bd98ad669b1aa75f253fb0342448a
jlucas <julie.lucas@telecom-paris.fr>
2019-11-25 20:47:52 +0100

    avance ihm, clarification JFrame

--- src/ihm/Window.java
+++ src/ihm/Window.java
@@ -6,20 +6,26 @@ import maze.*;
 
 public class Window extends JFrame {
 	
+	JFrame window;
 	private MazePanel pan;
+	private String Title;
+	//taille (en pixel)
+	private int longeur;
+	private int largeur;
+	private Maze maze;
 	
 	//interface graphique
 	public Window(Maze maze) {//constructeur
 		super();
-		JFrame window = new JFrame();
-		window.setTitle("Labyrinthe Hugo & Julie");//nom de la fenetre
-		window.setSize(1000, 800);//taille (en pixel)
+		window = new JFrame();
+		window.setTitle(Title);//nom de la fenetre
+		window.setSize(longeur, largeur);//taille (en pixel)
 		window.setLocationRelativeTo(null);//position centrale sur l'ecran
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//fin sur x
 		window.setResizable(false); //empeche redimentionnement de la fenetre
 		
 	    pan = new MazePanel(maze);
-	    //pan.setBackground(Color.BLUE);//couleur du JPanel
+	    pan.setBackground(Color.BLUE);//couleur du JPanel
 	    window.setContentPane(pan);// JPanel est le content pane
 		
 	    window.setVisible(true);
