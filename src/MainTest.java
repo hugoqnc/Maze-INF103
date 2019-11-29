f0e8801ebd01b1b68d1f6ce5beffef94068a544b
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-11-29 15:01:22 +0100

    new maze for exceptions test

--- src/MainTest.java
+++ src/MainTest.java
@@ -13,37 +13,10 @@ import ihm.Window;
 public class MainTest {
 
 	public static void main(String[] args) {
-	    Maze maze = new Maze("data/labyrinthe.txt");
-	    //a supprimer après test
-		//maze.initFromTextFile();
-	    /*VertexInterface depart = maze.getDepart();
-		VertexInterface arrivee = maze.getArrivee();
+	    Maze maze = new Maze("data/labyrinthe NON FONCTIONNEL.txt");
+	    
+		new Window("Labyrinthe", maze);
 		
-		//Dijkstra dij = new Dijkstra();
-		PreviousInterface previous = Dijkstra.dijkstra(maze, depart); //dijsktra est une m�thode de classe
-		ArrayList<VertexInterface> list = previous.getShortestPathTo(arrivee);
-		
-		for (int i=0; i<list.size(); i++) {
-			System.out.println(list.get(i).getLabel());
-		}*\
-		
-		//interface graphique
-/*		JFrame window = new JFrame();
-		window.setTitle("Labyrinthe binome 6");//nom de la fenetre
-		window.setSize(1000, 800);//taille (en pixel)
-		window.setLocationRelativeTo(null);//position centrée sur l'écran
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//fin sur x
-		window.setResizable(false); //empeche redimentionnement de la fenetre
-		
-		
-	    JPanel pan = new JPanel();
-	    pan.setBackground(Color.BLUE);//couleur du JPanel
-	    window.setContentPane(pan);// JPanel est le content pane   
-		
-	    window.setVisible(true);
-	    */
-		Window w = new Window("Labyrinthe", maze);
-		//Window b = new Window("Labyrinthe", maze);
 
 	}
 
--- src/maze/MazeReadingException.java
+++ src/maze/MazeReadingException.java
@@ -1,8 +1,9 @@
 package maze;
 
 public class MazeReadingException extends Exception {
-	public MazeReadingException(String fileName, int ligneError){
-		
+	public MazeReadingException(String fileName, int lineError, String message){
+		super("MazeReadingException at line " + lineError + ". Message : " + message);
+				
 		}
 	
 }
