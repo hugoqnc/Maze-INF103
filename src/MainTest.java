eb61a0867ce1c42559273d725cb93825b2b6eedf
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-12-03 16:58:55 +0100

    new DefaultMaze.txt for the window homescreen

--- src/MainTest.java
+++ src/MainTest.java
@@ -6,7 +6,7 @@ import ihm.Window;
 public class MainTest {
 
 	public static void main(String[] args) {
-	    Maze maze = new Maze("data/labyrinthe.txt");
+	    Maze maze = new Maze("data/DefaultMaze.txt");
 	    
 		new Window("Labyrinthe", maze);
 
