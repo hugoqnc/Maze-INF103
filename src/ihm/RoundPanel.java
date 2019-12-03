99c2dc997485bffb53974fd0d1b03d85904c8994
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-12-03 10:57:11 +0100

    fix paintComponent to have a nice path

--- src/ihm/RoundPanel.java
+++ src/ihm/RoundPanel.java
@@ -6,12 +6,12 @@ import javax.swing.*;
 public class RoundPanel extends JPanel {
 	
 	public void paintComponent(Graphics g) {
-		//super.paintComponent(g);
+		super.paintComponent(g);
+		setBackground(Color.WHITE);
 		int w = getWidth();
 		int h = getHeight();
+		g.setColor(Color.BLUE);
 		g.fillOval(w/4, h/4, w/2, h/2);
-		//setBackground(new Color(0,0,0,0)); //couleur transparente
-		//setPreferredSize(new Dimension(60,60));
 		
 	}
 
