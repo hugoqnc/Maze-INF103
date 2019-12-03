6357c4ab2800be6830ece44f57dbca0e4f170169
Julie Lucas <julie.lucas@telecom-paris.fr>
2019-12-04 00:31:34 +0100

    ActionListener début

--- src/ihm/EditPanel.java
+++ src/ihm/EditPanel.java
@@ -1,5 +1,13 @@
 package ihm;
 
-public class EditPanel {
+import java.awt.event.ActionListener;
 
+public class EditPanel extends JPanel implements ActionListener{
+
+	public EditPanel() {
+		super();
+	}
+	void actionPerformed(ActionEvent evt){
+		
+	}
 }
