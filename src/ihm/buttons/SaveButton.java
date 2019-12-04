9dfc5f2d27c8ce63f7f5716a6ddd2689e6894eba
Julie Lucas <jlucas@tp-1a201-06.enst.fr>
2019-12-04 16:35:54 +0100

    saveButton class

--- src/ihm/buttons/SaveButton.java
+++ src/ihm/buttons/SaveButton.java
@@ -1,5 +1,24 @@
 package ihm.buttons;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+//import MainTest;
 
-public class SaveButton {
+import ihm.*;
 
-}
+public class SaveButton extends JButton implements ActionListener{
+	
+	private Window window;
+	
+	public SaveButton(Window window) {
+		super("Save Maze");
+		addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent evt) {
+	    }
+	    
+		
+	}
