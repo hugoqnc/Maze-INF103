9fdc903c8c34dc26436bc49dbd5c2a51e0dab43c
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-12-05 00:16:25 +0100

    update maze homescreen

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,7 +7,7 @@ import ihm.Window;
 public class MainTest {
 
 	public static void main(String[] args) {
-	    Maze maze = new Maze("data/labyrinthe 2.txt");
+	    Maze maze = new Maze("data/DefaultMaze.txt");
 	    
 		new Window("Labyrinthe", maze);
 		
