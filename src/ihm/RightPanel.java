9bf06482345a08adbba7fc733f617c72ffc86d60
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2019-12-25 18:58:18 +0100

    Complete Manual in RightPanel

--- src/ihm/RightPanel.java
+++ src/ihm/RightPanel.java
@@ -11,12 +11,21 @@ public class RightPanel extends JPanel{
 	private JPanel menu;
 	
 	private String message = 
-			"<html><h2>Manual :</h2>"
-			+ "<h3>Edit :</h3>"
+			"<html><p></p>"
+			+ "<h1>Manual :</h1>"
+			+ "<h2>Load Maze :</h2>"
+			+ "<p>Select a compatible .txt file</p>"
+			+ "<h2>New Maze :</h2>"
+			+ "<P>Choose a name ending with .txt for the new maze</p>"
+			+ "<P>Then choose the length and width between 2 and 99</p>"
+			+ "<h2>Edit Maze :</h2>"
 			+ "<p>Left click to add/remove walls</p>"
 			+ "<p>Left click and maintain to add/remove several walls</p>"
 			+ "<P>Right click to add/remove a departure/arrival</p>"
-			+ "<h3>Load :</h3></html>";
+			+ "<h2>Save Maze :</h2>"
+			+ "<P>Saves the edited maze in the .txt file</p>"
+			+ "<h2>Resolve Maze :</h2>"
+			+ "<p>Saves the maze, then resolves it</p>";
 	private String title ="Edition Menu";
 	
 	public RightPanel() {//constructeur
