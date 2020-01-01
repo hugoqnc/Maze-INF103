c908e89db63ec469e54dea9e26c4c6f2ddcad541
Hugo Queinnec <hugo.queinnec@telecom-paris.fr>
2020-01-01 18:29:26 +0100

    fix a bug where a maze with an  error was saved while a popup said the contrary

--- src/ihm/buttons/SaveButton.java
+++ src/ihm/buttons/SaveButton.java
@@ -11,6 +11,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import ihm.*;
+import maze.Maze;
 
 public class SaveButton extends JButton implements ActionListener{
 	
@@ -27,8 +28,11 @@ public class SaveButton extends JButton implements ActionListener{
 	public void actionPerformed(ActionEvent evt) {
 		window.setMazeMode(0);
 			try{
-				fileName = window.getMaze().getFileName();
-				FileWriter editFile = new FileWriter(fileName,false);
+				//Methode : on cree une copie Copy du fichier txt comprenant la version editee du labyrinthe
+				//Si cette version ne contient pas de fautes, alors on remplace la version actuelle par la version editee, et on supprime le fichier Copy
+				//Si cette version contient une faute, alors on garde dans le fichier texte l'ancienne version sans fautes
+				String fileNameNew = fileName.substring(0,fileName.length() - 4) + "Copy.txt"; //change le nom "test.txt" en "testCopy.txt"
+				FileWriter editFile = new FileWriter(fileNameNew,false);
 				BufferedWriter editLector = new BufferedWriter(editFile);
 				for (int i=0; i<window.getMaze().getLongeur();i++) {
 					for (int j=0; j<window.getMaze().getLargeur();j++) {
@@ -37,8 +41,21 @@ public class SaveButton extends JButton implements ActionListener{
 				editLector.newLine();
 				}
 				editLector.close();
-				//le nouveau fichier est maintenant sauvegard�
-				window.changeMaze(fileName); //quitte le mode �dition
+				//le nouveau fichier Copy est maintenant sauvegarde
+				File oldFile = new File(fileName); 
+				File newFile = new File (fileNameNew);
+				try {
+					Maze mazeNew = new Maze(fileNameNew); //Permet de verifier si le nouveau fichier Copy contient une faute
+				}
+				catch(RuntimeException rte) {
+					newFile.delete(); //si oui, on le supprime et on stoppe l'execution de la methode
+					throw new RuntimeException("Error in the edited maze");
+				}
+				
+				newFile.renameTo(oldFile); //si non, on remplace le vieux fichier par le nouveau, et on supprime la copie
+				newFile.delete();
+				
+				window.changeMaze(fileName); //quitte le mode edition et affiche le labyrinthe normalement
 			}catch (IOException e) {
 				e.printStackTrace();
 				}
